5e1e69a9875ab8229ac7230a974f4d06e4ee1745
s098031 <tests098031@gmail.com>
2023-11-22 21:59:53 +0800

    20231122

---
+++ app/src/main/java/com/example/a112_1_mmslab_kotlin_lab07/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.a112_1_mmslab_kotlin_lab07
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private var progressRabbit = 0
+    private var progressTurtle = 0
+    private lateinit var btn_start: Button
+    private lateinit var sb_rabbit: SeekBar
+    private lateinit var sb_turtle: SeekBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_start = findViewById<Button>(R.id.btn_start)
+        sb_rabbit = findViewById<SeekBar>(R.id.sb_rabbit)
+        sb_turtle = findViewById<SeekBar>(R.id.sb_turtle)
+        btn_start.setOnClickListener(View.OnClickListener {
+            btn_start.setEnabled(false)
+            progressRabbit = 0
+            progressTurtle = 0
+            sb_rabbit.setProgress(0)
+            sb_turtle.setProgress(0)
+            runRabbit()
+            runTurtle()
+        })
+    }
+
+    private val handler = Handler(
+        Looper.myLooper()!!
+    ) { msg ->
+        if (msg.what == 1) sb_rabbit!!.progress =
+            progressRabbit else if (msg.what == 2) sb_turtle!!.progress =
+            progressTurtle
+        if (progressRabbit >= 100 && progressTurtle < 100) {
+            Toast.makeText(this@MainActivity, "兔子勝利", Toast.LENGTH_SHORT).show()
+            btn_start!!.isEnabled = true
+        } else if (progressRabbit < 100 && progressTurtle >= 100) {
+            Toast.makeText(this@MainActivity, "烏龜勝利", Toast.LENGTH_SHORT).show()
+            btn_start!!.isEnabled = true
+        }
+        false
+    }
+
+    private fun runRabbit() {
+        Thread {
+            val sleepProbability = booleanArrayOf(true, true, false)
+            while (progressRabbit <= 100 && progressTurtle < 100) {
+                try {
+                    Thread.sleep(100)
+                    if (sleepProbability[(Math.random() * 3).toInt()]) {
+                        Thread.sleep(300)
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                progressRabbit += 3
+                val msg = Message()
+                msg.what = 1
+                handler.sendMessage(msg)
+            }
+        }.start()
+    }
+
+    private fun runTurtle() {
+        Thread {
+            while (progressRabbit < 100 && progressTurtle <= 100) {
+                try {
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                progressTurtle += 1
+                val msg = Message()
+                msg.what = 2
+                handler.sendMessage(msg)
+            }
+        }.start()
+    }
+}
